2dbe8e2ba35827108f3b7fa8fee607d36fd69954
gustavo-pimenta <63674712+gustavo-pimenta@users.noreply.github.com>
2020-07-01 17:01:48 -0300

    new direct input version

---
+++ Direct Input version/PyDirectInput_test.py
@@ -0,0 +1,18 @@
+import pyautogui
+import pydirectinput
+import time
+
+# pydirectinput.moveTo(100, 150) # Move the mouse to the x, y coordinates 100, 150.
+#  # Click the mouse at its current location.
+# pydirectinput.click(200, 220) # Click the mouse at the x, y coordinates 200, 220.
+  # Move mouse 10 pixels down, that is, move the mouse relative to its current position.
+# pydirectinput.doubleClick() # Double click the mouse at the
+# pydirectinput.press('esc') # Simulate pressing the Escape key.
+# pydirectinput.keyDown('shift')
+# pydirectinput.keyUp('shift')
+
+while True:
+    # pydirectinput.move(None, 30)
+    # pydirectinput.click()
+    pydirectinput.press('w')
+    time.sleep(1)
